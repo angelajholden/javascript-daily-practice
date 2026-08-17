c98fb1701fe4f74110134c697732527666befc75
Angela J Holden <angelajholden@gmail.com>
2026-08-17 15:53:23 -0500

    reverse a string 3 ways

--- js/15_reverse-a-string.js
+++ js/15_reverse-a-string.js
@@ -1,8 +1,37 @@
-function reverseAString() {
-	let string = "hello";
-	string = string.split("", -1);
+function reverseAString(str) {
+	// built in toReversed() method
+	let string = str;
+	string = string.split("");
 	string = string.toReversed();
 	string = string.join("");
-	console.log(string);
+	// console.log(string);
+
+	let reversed = ""; // initialize reversed
+	// i is equal to the length of the str minus 1
+	// run the loop if i is greater than or equal to 0
+	// subtract 1 from i each time the loop runs
+	for (let i = str.length - 1; i >= 0; i--) {
+		reversed += str[i];
+		// reversed = reversed + str[i]
+		// str[i] is each letter
+	}
+	// console.log(reversed);
+
+	let word = ""; // initialize word as empty string
+	for (const char of str) {
+		// define char and loop through each character
+
+		word = char + word;
+		// put each new character in front of the string built so far
+	}
+	console.log(word);
+
+	// return all the things
+	return {
+		reversed,
+		word,
+	};
 }
-reverseAString();
+reverseAString("hello");
+reverseAString("heidi");
+reverseAString("wonderful");
